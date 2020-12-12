f46edba0993f2b8d869fb1835cbc589c3e1af7f7
JInwook0823 <53897233+JInwook0823@users.noreply.github.com>
2020-12-13 04:25:48 +0900

    step-2 클래스 생성

---
+++ src/step_2.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class step_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char cubeArr[][] = {{'R','R','W'}, {'G','C','W'}, {'G', 'B', 'B'}};
+	}
+}
