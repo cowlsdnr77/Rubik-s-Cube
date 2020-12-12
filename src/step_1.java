5ba1a7feaef7f607631cb6d68f7510fd14e9299f
JInwook0823 <53897233+JInwook0823@users.noreply.github.com>
2020-12-13 03:47:31 +0900

    direction 값에 따라 메소드 호출
    - 추가로 입력받기 전 ">" 문자 추가

--- src/step_1.java
+++ src/step_1.java
@@ -2,12 +2,23 @@ import java.util.Scanner;
 public class step_1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.print("> ");
 		Scanner input = new Scanner(System.in);
 		String word = input.next();
 		int number = input.nextInt();
 		String direction = input.next().toUpperCase(); //방향 알파벳을 모두 대문자로 바꾼다
 		input.close();
 		String[] WordArr = word.split(""); //단어를 문자 하나씩 배열에 저장
+		
+		if(direction.equals("L")) {
+			String[] resultArr = moveLeft(WordArr, WordArr.length, number);
+			System.out.println(String.join("",resultArr));	
+		} else if(direction.equals("R")) {
+			String[] resultArr = moveRight(WordArr, WordArr.length, number);
+			System.out.println(String.join("",resultArr));
+		} else {
+			System.out.println("wrong direction");
+		}
 	}
 	
 	//왼쪽 밀어내기
