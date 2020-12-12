1b6b6dbdc614b133653b88ffae5ff8cef212b144
JInwook0823 <53897233+JInwook0823@users.noreply.github.com>
2020-12-13 04:44:41 +0900

    moveOrder 메소드 추가
    -moveOrder: 입력받은 Arr을 ArrayList에 담고 ArrayList의 값 중 '을 이전 값과 더해서 ArrayList를 리턴한다.

--- src/step_2.java
+++ src/step_2.java
@@ -18,6 +18,10 @@ public class step_2 {
 				System.out.println("Bye~");
 				input.close();
 				break;
+			} else {
+				String[] moveArr = moveInput.split("");
+				ArrayList<String> moveArray = moveOrder(moveArr);
+				System.out.println("");
 			}
 		}
 	}
@@ -31,5 +35,21 @@ public class step_2 {
 			System.out.println("");
 		}
 		System.out.println();
+	}
+	
+	//입력 arraylist에 담기
+	//ArrayList의 값 중 '와 같다면 그 이전 값에 '를 합친 값을 넣고 ArrayList의 마지막 값(중복된 값)을 삭제
+	public static ArrayList<String> moveOrder (String Arr[]) {
+		ArrayList<String> moveArray = new ArrayList<>(Arrays.asList(Arr));
+		for(int i=1 ; i <moveArray.size(); i++) {
+			if(moveArray.get(i).equals("'")) { 
+				moveArray.set(i-1, moveArray.get(i-1)+moveArray.get(i));
+				for(int j=i ; j<moveArray.size()-1 ; j++) {
+					moveArray.set(j, moveArray.get(j+1));
+				}
+				moveArray.remove(moveArray.size()-1);
+			}
+		}
+		return moveArray;
 	}
 }
