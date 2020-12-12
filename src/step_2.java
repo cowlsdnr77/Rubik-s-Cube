50bafa8bb228886b0254ea95c489d06d591bf399
JInwook0823 <53897233+JInwook0823@users.noreply.github.com>
2020-12-13 04:29:24 +0900

    showCube 메소드 추가
    -showCube: 3x3 평면큐브를 콘솔창에 출력

--- src/step_2.java
+++ src/step_2.java
@@ -7,5 +7,17 @@ public class step_2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		char cubeArr[][] = {{'R','R','W'}, {'G','C','W'}, {'G', 'B', 'B'}};
+		showCube(cubeArr);
+	}
+	
+	//큐브 출력 함수
+	public static void showCube (char Arr[][]) {
+		for(int i = 0; i < 3 ; i++) {
+			for(int j = 0; j < 3 ; j++) {
+				System.out.print(Arr[i][j] + " ");
+			}
+			System.out.println("");
+		}
+		System.out.println();
 	}
 }
