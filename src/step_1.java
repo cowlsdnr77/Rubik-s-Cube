4b30effa7a1367d1067cbfd8226dce85678551fb
JInwook0823 <53897233+JInwook0823@users.noreply.github.com>
2020-12-13 03:38:08 +0900

    moveLeft, moveRight 메소드 추가
    - moveLeft: 왼쪽으로 밀어내는 기능 수행
    - moveRight: 오른쪽으로 밀어내는 기능 수행

--- src/step_1.java
+++ src/step_1.java
@@ -9,4 +9,36 @@ public class step_1 {
 		input.close();
 		String[] WordArr = word.split(""); //단어를 문자 하나씩 배열에 저장
 	}
+	
+	//왼쪽 밀어내기
+	static String[] moveLeft(String[] Arr, int length, int moves) {
+		if(moves < 0) { //음수일 경우 R과 같다
+			return moveRight(Arr, length, -moves);
+		} else {
+			for(int i=0 ; i < moves ; i++) {
+				String tmp = Arr[0];
+				for(int j=0 ; j <length-1 ; j++) {
+					Arr[j] = Arr[j+1];
+				}
+				Arr[length-1] = tmp;
+			}
+			return Arr;
+		}
+	}
+		
+	//오른쪽 밀어내기
+	static String[] moveRight(String[] Arr, int length, int moves) {
+		if(moves < 0) { //음수일 경우 L과 같다
+			return moveLeft(Arr, length, -moves);
+		} else {
+			for(int i=0 ; i<moves ; i++) {
+				String tmp = Arr[length-1];
+				for(int j=length-1 ; j>0 ; j--) {
+					Arr[j] = Arr[j-1];
+				}
+				Arr[0] = tmp;
+			}
+			return Arr;
+		}
+	}
 }
