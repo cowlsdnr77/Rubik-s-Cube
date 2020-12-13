ba9b78b2d181d75a781a59722f1ad11eca0a4e63
JInwook0823 <53897233+JInwook0823@users.noreply.github.com>
2020-12-13 13:49:51 +0900

    moveOrder 메소드 추가
    moveOrder: 입력받은 배열을 ArrayList에 저장

--- src/step_3.java
+++ src/step_3.java
@@ -12,8 +12,22 @@ public class step_3 {
 		char leftArr[][] = {{'Y','Y','Y'}, {'Y','Y','Y'}, {'Y', 'Y', 'Y'}};
 		char topArr[][] = {{'B','B','B'}, {'B','B','B'}, {'B', 'B', 'B'}};
 		char bottomArr[][] = {{'R','R','R'}, {'R','R','R'}, {'R', 'R', 'R'}};
+		String moveInput;
 		
 		showCube(frontArr, backArr, rightArr, leftArr, topArr, bottomArr);
+		
+		while(true) {
+			System.out.print("\nCUBE> ");
+			Scanner input = new Scanner(System.in);
+			moveInput = input.next(); //엔터 두번 쳐야함
+			if(moveInput.equals("Q")) {
+				input.close();
+				break;
+			} else {
+				String[] moveArr = moveInput.split("");
+				ArrayList<String> moveArray = moveOrder(moveArr);
+			}
+		}
 	}
 	
 	//큐브 위, 아래 단면 출력
@@ -57,5 +71,33 @@ public class step_3 {
 		showHorizontalSide(backArr,rightArr,frontArr,leftArr);
 		System.out.println("");
 		showVerticalSide(bottomArr);
+	}
+	
+	//입력받은 배열을 arraylist에 담기
+	public static ArrayList<String> moveOrder (String Arr[]) {
+		ArrayList<String> moveArray = new ArrayList<>(Arrays.asList(Arr));
+		
+		//'으로 회전 명령 나누기
+		for(int i=1 ; i <moveArray.size() ; i++) {
+			if(moveArray.get(i).equals("'")) {
+				moveArray.set(i-1, moveArray.get(i-1)+moveArray.get(i));
+				for(int j=i ; j<moveArray.size()-1 ; j++) {
+					moveArray.set(j, moveArray.get(j+1));
+				}
+				moveArray.remove(moveArray.size()-1);
+			}
+		}
+		
+		//회전횟수(숫자)만큼 회전 명령을 추가한다
+		for(int i=1 ; i<moveArray.size(); i++) {
+			if(Character.isDigit(moveArray.get(i).charAt(0))) {
+				int tmp = Integer.parseInt(moveArray.get(i));
+				moveArray.set(i, moveArray.get(i-1));
+				for(int j=1 ; j<tmp-1 ; j++) {
+					moveArray.add(i, moveArray.get(i-1));
+				}
+			}
+		}
+		return moveArray;
 	}
 }
